467defe53bac84503c36a1ad591b258d6e736059
--global <--global>
2022-04-04 00:26:51 +0900

    animation splash screen

--- src/components/home/Splash.tsx
+++ src/components/home/Splash.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { useTexture } from "@react-three/drei";
-import { Suspense, VFC } from "react";
+import { useFrame } from "@react-three/fiber";
+import { Suspense, useRef, VFC } from "react";
+import { ShaderMaterial } from "three";
 import splashSrc from "./splash.png";
 
 const shaders = {
@@ -18,23 +20,34 @@ varying vec2 vUv;
 
 void main() {
   float v = mod(floor(vUv.x * 20.0 - vUv.y * 20.0), 2.0);
-  vec2 uv = vUv + vec2(1.0, 1.0) * (v * 2.0 - 1.0) * progress * delta;
-  gl_FragColor = texture2D(tex, uv) * mix(vec4(1.0), vec4(1.0, 0.0, 0.0, 1.0), mod(v, 2.0));
+  float p = exp(-progress * 4.0);
+  vec2 uv = vUv + vec2(1.0, 1.0) * (v * 2.0 - 1.0) * (1.0 - p) * delta;
+  gl_FragColor = texture2D(tex, uv) * vec4(1.0, 1.0, 1.0, clamp(0.0, 1.0, p));
 }
   `,
 };
 
 export const SplashInner: VFC<Record<string, any>> = ({ ...rest }) => {
   const tex = useTexture(splashSrc.src);
+  const t = useRef(0);
+  const matRef = useRef<ShaderMaterial>(null)
+
+  useFrame((_, delta) => {
+    if (!matRef.current) return;
+    t.current += delta;
+    // console.clear()
+    matRef.current.uniforms.progress.value = Math.max(0.0, t.current - 2.0);
+  })
   return (
     <mesh {...rest}>
       <planeBufferGeometry />
       <shaderMaterial
+        ref={matRef}
         transparent
         uniforms={{
           tex: { value: tex },
-          progress: { value: 0.5 },
-          delta: { value: 0.02 },
+          progress: { value: 0 },
+          delta: { value: 0.05 },
         }}
         {...shaders}
       />
